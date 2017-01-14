9d20911e41972495378d3d8df8a15ce135e7c653
ani7rudh <yadavanirudh6@gmail.com>
2017-01-14 15:23:49 +0530

    project 6
    project 6

---
+++ TechFab/src/main/java/com/electro/daoimpl/CartItemDAOImpl.java
@@ -0,0 +1,54 @@
+package com.electro.daoimpl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.electro.dao.CartItemDAO;
+import com.electro.model.CartItem;
+import com.electro.model.Category;
+
+@Repository
+
+public class CartItemDAOImpl implements CartItemDAO{
+	
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public CartItemDAOImpl(SessionFactory sessionFactory) {
+		this.sessionFactory=sessionFactory;
+	}
+
+
+	public void addCartItem(CartItem cartItem) {
+		sessionFactory.getCurrentSession().saveOrUpdate(cartItem);
+
+	}
+	
+public CartItem getCartListById(int cartItemId) {
+		
+		Session session=sessionFactory.getCurrentSession();
+		String hql="from CartItem where cartItemId="+cartItemId;
+		@SuppressWarnings("unchecked")
+		List<CartItem> cilist=session.createQuery(hql).list();
+		return cilist.get(0);
+	}
+	
+	
+public void updateFlag(int cartItemId)
+{
+	
+	String hql="update CartItem set flag=true where cartItemId="+cartItemId;
+	
+	sessionFactory.getCurrentSession().createQuery(hql).executeUpdate();
+
+}
+
+
+	
+
+
+}
